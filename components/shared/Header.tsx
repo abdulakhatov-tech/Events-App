52f9b80375101679f4a1ff6f9043c98b2f4d5f75
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-12-25 14:23:13 +0500

    header & clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { SignIn, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
 import { Button } from '../ui/button';
+import NavItems from './NavItems';
+import MobileNav from './MobileNav';
 
 const Header = () => {
   return (
@@ -17,10 +21,23 @@ const Header = () => {
           />
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
-          <Button asChild className="rounded-full" size="lg">
-            <Link href="/sign-in">Login</Link>
-          </Button>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </header>
